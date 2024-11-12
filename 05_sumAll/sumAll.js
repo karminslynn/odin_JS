95a1650be704daa734e37eb3190becfa8decfe6b
Karminsgit <karminsalynn@outlook.com>
2024-11-12 14:25:40 +0300

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(p, q) {
+    // Check for invalid inputs
+    if (!(Number.isInteger(p) && Number.isInteger(q)) || (p < 0 || q < 0)) {
+      return "ERROR";
+    }
+  
+    // Get the range of numbers (min and max)
+    const [min, max] = p > q ? [q, p] : [p, q];
+  
+    // Calculate the sum of all integers between min and max (inclusive)
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+  
+    return sum;
+  };
 
-};
 
-// Do not edit below this line
+// npm test sumAll.spec.js
 module.exports = sumAll;
