4bf4101ae9f36efa5f5b72393dcffd57685a0585
Karminsgit <karminsalynn@outlook.com>
2024-11-12 15:55:54 +0300

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,67 @@
-const add = function() {
+const add = function(a,b) {
+
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+
+  return a-b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+
+let total=0;
+arr.forEach(element => {
+  total+=element;
+});
+
+return total;
 };
 
-const multiply = function() {
+const multiply = function(number) {
+
+
+  
+
+
+
+  let total=1;
+  number.forEach(element => {
+    total=total*element;
+  });
+  
+  return total;
+
+
+
+
+
+
+
+
+
+
+
 
 };
 
-const power = function() {
+const power = function(a,b) {
 	
+
+return a**b;
+
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+let total=1;
+  if(a===0) return 1;
+  while(a){
+  total=total*a;
+  a--;
+  }
+  return total;
 };
 
 // Do not edit below this line
