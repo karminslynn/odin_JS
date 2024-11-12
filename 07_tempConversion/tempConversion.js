b33b446f8cb3bec072cf6a5ab9f27993eb8da913
Karminsgit <karminsalynn@outlook.com>
2024-11-12 15:30:44 +0300

    completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,33 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+
+let C = (5/9)*(F-32);
+
+return Number(C.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+
+
+let F = C*(9/5)+32;
+
+
+return Number(F.toFixed(1));
+
 };
 
-// Do not edit below this line
+
+
+
+
+
+
+
+
+
+
+
+// npm test tempConversion.spec.js
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
